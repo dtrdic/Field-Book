b1d62a496087c6923eb9082ed637924c8f896871
Nahuel Soldevilla <n.soldevilla@integratedbreeding.net>
2025-08-01 16:42:29 -0300

    IBP-7142 try-4, fix codemagic app preview, ios app launch

--- iosApp/Sources/App.swift
+++ iosApp/Sources/App.swift
@@ -5,14 +5,45 @@ import sharedKit
 struct IosApp: App {
     var body: some Scene {
         WindowGroup {
-            ComposeView()
+            ContentView()
         }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea()
     }
 }
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainKt.MainViewController()
+       do {
+           let controller = MainKt.MainViewController()
+           return controller
+       } catch {
+           print("Error creating Compose view controller: \(error)")
+           // Return a fallback view controller
+           let fallbackController = UIViewController()
+           fallbackController.view.backgroundColor = .systemBackground
+
+           let label = UILabel()
+           label.text = "Error loading KMP module: \(error.localizedDescription)"
+           label.textAlignment = .center
+           label.numberOfLines = 0
+           label.translatesAutoresizingMaskIntoConstraints = false
+
+           fallbackController.view.addSubview(label)
+           NSLayoutConstraint.activate([
+               label.centerXAnchor.constraint(equalTo: fallbackController.view.centerXAnchor),
+               label.centerYAnchor.constraint(equalTo: fallbackController.view.centerYAnchor),
+               label.leadingAnchor.constraint(greaterThanOrEqualTo: fallbackController.view.leadingAnchor, constant: 20),
+               label.trailingAnchor.constraint(lessThanOrEqualTo: fallbackController.view.trailingAnchor, constant: -20)
+           ])
+
+           return fallbackController
+       }
     }
     func updateUIViewController(_: UIViewController, context: Context) {}
 }
